ab1d8b9634746fba4523425c77e5cd3f4151cf31
Shlac <126964688+Shlac@users.noreply.github.com>
2023-06-15 17:33:24 +0200

    Add files via upload

--- imdex.js
+++ imdex.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   const textoCambiante = document.getElementById('texto-cambiante');
-  const palabras = ["gratis", "divertidas", "entretenidas", "pro", "fachas", "aventureras", "interesantes"];
+  const palabras = ["gratis", "divertidas", "entretenidas", "pro", "fachas", "aventureras", "interesantes","emocionantes","fachas","insanas"];
   let indicePalabra = 0;
   let indiceLetra = 0;
   let primeraLetraMostrada = false;
@@ -47,4 +47,56 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   cambiarPalabra();
+});
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var inputBusqueda = document.getElementById('busqueda');
+  var buscarBtn = document.getElementById('buscarBtn');
+  var borrarBtn = document.getElementById('borrarBtn');
+
+  function buscarPeliculas() {
+    var valorBusqueda = inputBusqueda.value.toLowerCase();
+    var peliculas = document.getElementById('peliculas');
+    
+    if (peliculas) {
+      var peliculasEnlaces = peliculas.getElementsByTagName('a');
+      
+      for (var i = 0; i < peliculasEnlaces.length; i++) {
+        var tituloPelicula = peliculasEnlaces[i].textContent.toLowerCase();
+        
+        if (tituloPelicula.indexOf(valorBusqueda) > -1) {
+          peliculasEnlaces[i].style.display = '';
+        } else {
+          peliculasEnlaces[i].style.display = 'none';
+        }
+      }
+    }
+  }
+
+  function limpiarBusqueda() {
+    inputBusqueda.value = '';
+    buscarPeliculas();
+    inputBusqueda.focus();
+  }
+
+  buscarBtn.addEventListener('click', buscarPeliculas);
+  inputBusqueda.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13) { // Código de tecla "Enter"
+      buscarPeliculas();
+    }
+  });
+
+  borrarBtn.addEventListener('click', limpiarBusqueda);
+
+  inputBusqueda.addEventListener('input', function() {
+    if (inputBusqueda.value !== '') {
+      borrarBtn.style.display = 'block';
+    } else {
+      borrarBtn.style.display = 'none';
+    }
+  });
 });
